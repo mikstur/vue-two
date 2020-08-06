5e5f12f9b7136406a49e8f1c797c974df4121145
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2020-08-06 18:48:15 +0200

    Some data binding and methods and life cycles

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-bind:id="messageId">{{ message }}</div>
+    <div>{{ appendSomething }} - {{ sum(1, 2) }}</div>
+
+    <div> {{orderedOn}} </div>
+
+    <div> {{prettyOrderedOn}} </div>
+
+    <div v-for="item of items" :key="item.id">
+      {{item.title}}
+    </div>
   </div>
 </template>
 
@@ -10,8 +20,55 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      messageId: "message-id123",
+      message: "Test Message test",
+      orderedOn: new Date(),
+      items: [
+        {
+          id: 1,
+          title: "Hello"
+        },
+        {
+          id: 2,
+          title: "Hello"
+        },
+        {
+          id: 3,
+          title: "Hello"
+        }
+      ]
+    }
+  },
+  created: function () {
+    console.log("Lets fetch some data from Firebase...");
+    this.fetchItems();
+  },
+  computed: {
+    appendSomething: function () {
+      return this.message + " something";
+    },
+    prettyOrderedOn: function() {
+      return this.orderedOn.getMonth() + " - " + this.orderedOn.getFullYear()
+    }
+  },
+  methods: {
+    sum: function(a, b) {
+      return a + b;
+    },
+    fetchItems: function() {
+      // Go to firebase 
+      this.items = [
+        {
+          id: 1,
+          title: "Hello"
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
